250401bd06369e289322c374992b886abe275bf4
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-08 10:06:30 -0300

    pageheader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from "../template/pageHeader"
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='a Aplicação' ></PageHeader>
+
+        <h2>Siaba mais sobre o To-Do</h2>
+        <p>A empresa do To-Do começou quandose soube que a população precisava caçar o que fazer.</p>
+        <h2>Missão e Visão</h2>
+        <p>A missão é ajudar as pessoas a fazer com mais zelo suas atividades, e a visão tá boa.</p>
+        <h2>Imprensa</h2>
+        <p>Nem sei o que dizer</p>
     </div>
 )
