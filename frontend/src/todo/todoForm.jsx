546b85c090c71baf9ef643ba3655c082b1d1f85c
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-08 15:06:47 -0300

    grid e iconButtons

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
+export default props => (
+    <div role='form' className='todoForms'>
+        <Grid cols= '12 9 10'>
+            <input id='description' className='form-control' placeholder='Adicione uma terefa'></input>
+        </Grid>
+        <Grid cols= '12 3 2'>
+            <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>
+    </div>
+)
