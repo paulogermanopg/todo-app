abd94a1653f604ee4f383137094edf8d379157bc
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-09 14:21:28 -0300

    misturando com o back

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,12 +3,16 @@ import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
 export default props => (
-    <div role='form' className='todoForms'>
-        <Grid cols= '12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma terefa'></input>
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control'
+                placeholder='Adicione uma terefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
         <Grid cols= '12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
