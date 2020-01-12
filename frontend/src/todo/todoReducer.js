2660bb1f28235a02fd22f1b827c2ebc668ba1a4d
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-12 20:00:08 -0300

    adicionando a middewere

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-    description: 'Ler meu Livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Não pagar o cartão',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Fazer lista',
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
