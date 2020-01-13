01a9a7f19014abb646d209a47dc0653737093abe
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-01-13 13:59:02 -0300

    adicionando duas middewares para search e add

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,11 +4,14 @@ import App from './main/app'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
+
+import multi from 'redux-multi'
 import promise from 'redux-promise'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
